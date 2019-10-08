5f616e5136c606360bf6d8c8f8051e0128db1a0a
Polly Michel <47877075+pmichel16@users.noreply.github.com>
2019-10-08 10:42:21 -0400

    Started working on scripting

--- snake-game/snakeScript.js
+++ snake-game/snakeScript.js
@@ -1,4 +1,29 @@
-function handleKey(e) {
-    var keycode = (e.keyCode ? e.keyCode : e.which);
-    alert('Keycode = ' + keycode);
+function startGame() {
+  if (confirm('Press OK to begin!')) {
+    var head = document.getElementById("snake-head");
+    head.style.visibility = "visible";
+    moveHead();
+  }
+}
+
+function moveHead() {
+  const head = document.getElementById("snake-head");
+  const headStyle = getComputedStyle(head);
+  const headLeft = headStyle.left;
+  var pos = parseFloat(headStyle.left);
+  var id = setInterval(frame,5);
+
+  alert(head.style.left);
+
+  function frame( ) {
+    if (pos > 40) {
+      clearInterval(id);
+    } else {
+      pos+=0.5;
+      head.style.left = pos + 'px';
+    }
+  }
+
+  alert(head.style.left);
+  
 }
