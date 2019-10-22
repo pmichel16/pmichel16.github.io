51079a5b84d571ec8f3ed7ca89dffa8692314920
Polly Michel <pmichel928@gmail.com>
2019-10-21 21:57:50 -0400

    Completed snake head moving animation.

--- snake-game/snakeScript.js
+++ snake-game/snakeScript.js
@@ -4,23 +4,80 @@ function startGame() {
   moveHead();
 }
 
-function moveHead() {
+function getDirection(event) {
+  if (event.keyCode == 37 || event.keyCode == 38 || event.keyCode == 39 || event.keyCode == 40) {
+    moveHead(event.keyCode);
+  }
+}
+
+function moveHead(dirKey = 39) {
+  var dir = new String;
+  //Convert the keypress value to a string for changing properties later.
+  switch (dirKey) {
+    case 37:
+      dir = "left";
+      break;
+    case 38:
+      dir = "up";
+      break;
+    case 39:
+      dir = "right";
+      break;
+    case 40:
+      dir = "down";
+      break;
+    default:
+  }
   const vmin = Math.min(document.documentElement.clientWidth, document.documentElement.clientHeight);
   const head = document.getElementById("snake-head");
-  const headLeft = getComputedStyle(head).left;
-  var pos = parseFloat(headLeft);
+  var headEdge = new Object;
+  const moveX = (dir == "left" || dir == "right");
+  if (moveX) {
+    headEdge = getComputedStyle(head).left;
+  } else {
+    headEdge = getComputedStyle(head).top;
+  }
+  var pos = parseFloat(headEdge);
   //Each square is 7.5vmin by 7.5vmin, so find when the circle has moved one square.
-  const posPlus = vmin * 0.075 + pos;
-  var id = setInterval(frame, 10);
+  const posPlus = pos + vmin * 0.075;
+  const posMinus = pos - vmin * 0.075;
 
-  function frame( ) {
+  //If right or down, move "forwards", otherwise move "backwards".
+  var animate;
+  if (dir == "right" || dir == "down") {
+    animate = setInterval(framePlus, 10);
+  } else {
+    animate = setInterval(frameMinus, 10);
+  }
+
+  function framePlus() {
     if (pos > posPlus) {
       pos = posPlus;
-      clearInterval(id);
+      clearInterval(animate);
     } else {
-      pos+=1.5;
+      pos += 1.5;
     }
-    head.style.left = pos + 'px';
+    //Update the position of the head based on pos
+    if (moveX) {
+      head.style.left = pos + 'px';
+    } else {
+      head.style.top = pos + 'px';
+    }
   }
+  function frameMinus() {
+    if (pos < posMinus) {
+      pos = posMinus;
+      clearInterval(animate);
+    } else {
+      pos -= 1.5;
+    }
+    //Update the position of the head based on pos
+    if (moveX) {
+      head.style.left = pos + 'px';
+    } else {
+      head.style.top = pos + 'px';
+    }
+  }
+
+};
   
-}
