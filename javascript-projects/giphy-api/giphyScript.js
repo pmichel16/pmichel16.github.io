f39e7fea37174081f64bf870835a5e63398d3618
Polly Michel <47877075+pmichel16@users.noreply.github.com>
2019-10-31 14:12:30 -0400

    Added number of images functionality and css

--- javascript-projects/giphy-api/giphyScript.js
+++ javascript-projects/giphy-api/giphyScript.js
@@ -1,8 +1,17 @@
 function giphySearch() {
-	const searchInput=$("#searchBox").val();
-	var searchString=searchInput.replace(/ /g, "+");
-	var request = $.get('https://api.giphy.com/v1/gifs/search?q='+searchString+'&api_key=OM5EygOcdpf80lr9XF7OObeEUsUOVUJ7&limit=5"',true);
-	request.done(function(data) { 
-		console.log("success got data", data); 
-	});
+  const searchInput=$("#search-box").val();
+  var results=$("#num-results").val();
+  var searchString=searchInput.replace(/ /g, "+");
+  var request = $.get('https://api.giphy.com/v1/gifs/search?q='+searchString+'&api_key=OM5EygOcdpf80lr9XF7OObeEUsUOVUJ7&limit='+results.toString()+'"',true);
+  request.done(function(dataReceived) { 
+
+    //Delete any images that already exist
+    $(".gif-result").remove();
+
+    //Populate results-container with images
+    for(var i = 0; i < results; i++) {
+	$("#results-container").append($("<img>").prop({"src":dataReceived.data[i].images.fixed_height.url,"id":"gif-"+i}));
+        $("#gif-"+i).addClass("img-responsive gif-result");
+    }
+  });
 };
