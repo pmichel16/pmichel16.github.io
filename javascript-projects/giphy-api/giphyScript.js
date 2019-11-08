51c6154e294fcdcced1efd3bc2d844d7e2e40c9d
Polly Michel <47877075+pmichel16@users.noreply.github.com>
2019-11-07 22:15:48 -0500

    Shows trending gifs when opening page

--- javascript-projects/giphy-api/giphyScript.js
+++ javascript-projects/giphy-api/giphyScript.js
@@ -1,40 +1,32 @@
-var reqURL = "";
-var resPerPage = 0;
-var hasSearched = false;
-var searchString = "";
-
-function giphySearch() {
+var offset=0;
+function makeURL(offset) {
   const searchInput=$("#search-box").val();
-  resPerPage = Number($("#num-results").val());
-  searchString=searchInput.replace(/ /g, "+");
-  reqURL = 'https://api.giphy.com/v1/gifs/search?q=' + searchString + '&api_key=OM5EygOcdpf80lr9XF7OObeEUsUOVUJ7&limit=' + resPerPage.toString() + '"';
-  $.get(reqURL, function(dataReceived) {
-    console.log(dataReceived);
-    //Delete any images that already exist
-    $(".gif-result").remove();
+  const resPerPage = Number($("#num-results").val());
+  var reqURL = "";
+  if(searchInput!="") {
+    const searchString = $("#search-box").val().replace(/ /g, "+");
+    reqURL = 'https://api.giphy.com/v1/gifs/search?q=' + searchString + '&api_key=OM5EygOcdpf80lr9XF7OObeEUsUOVUJ7&limit=' + resPerPage.toString() + '&offset=' + offset.toString() + '"';
+  } else {
+    reqURL = 'https://api.giphy.com/v1/gifs/trending?api_key=OM5EygOcdpf80lr9XF7OObeEUsUOVUJ7&limit=' + resPerPage.toString() + '&offset=' + offset.toString() + '"'
+  }
+  return reqURL;
+}
+
+function getGifs(offset) {
+  const resPerPage = Number($("#num-results").val());
+  const url=makeURL(offset);
+  $.get(url, function(data) {
+    //If offset is 0, then the search is starting new and we want to delete any extra loaded images
+    if(offset===0) $(".gif-result").remove();
 
     //Populate results-container with images
     for(var i = 0; i < resPerPage; i++) {
-	$("#results-container").append($("<img>").prop({"src":dataReceived.data[i].images.fixed_height.url,"id":"gif-"+i}));
-        $("#gif-"+i).addClass("gif-result");
-    }
-
-    //Show the load more button
-    $("#load-button").css("visibility", "visible");
-
-    //Allow changing the results per page to re-run the function
-    hasSearched = true;
-  });
-};
-
-function loadMore() {
-  resPerPage += Number($("#num-results").val());
-  reqURL = 'https://api.giphy.com/v1/gifs/search?q=' + searchString + '&api_key=OM5EygOcdpf80lr9XF7OObeEUsUOVUJ7&limit=' + resPerPage.toString() + '"';
-  $.get(reqURL, function (dataReceived){
-    //Populate only the new images into the results-container
-    for (var i = resPerPage - Number($("#num-results").val()); i < resPerPage; i++) {
-      $("#results-container").append($("<img>").prop({ "src": dataReceived.data[i].images.fixed_height.url, "id": "gif-" + i }));
-      $("#gif-" + i).addClass("img-responsive gif-result");
+$("#results-container").append($("<img>").prop("src",data.data[i].images.fixed_height.url).addClass("gif-result"));
     }
   })
+}
+
+function loadMore() {
+  offset+=Number($("#num-results").val());
+  getGifs(offset);
 }
