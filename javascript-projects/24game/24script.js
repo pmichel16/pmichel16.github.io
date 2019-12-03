82a4dbe36130b7f435b6afe7011a577f308b685e
Polly Michel <47877075+pmichel16@users.noreply.github.com>
2019-12-03 14:50:00 -0500

    Validate form, find permutations

--- javascript-projects/24game/24script.js
+++ javascript-projects/24game/24script.js
@@ -1,6 +1,20 @@
-function solve24 (numStr) {
-  var nums = numStr.split("");
-  nums = nums.map((x => Number.parseFloat(x).toFixed(0)));
+function validate(){
+  const inputs = document.getElementsByClassName("num-input");
+  $(".error-msg").css("visibility","hidden");
+  var valid = true;
+  for(var i = 0; i < inputs.length; i++) {
+    if(isNaN(parseInt(inputs[i].value)) || parseInt(inputs[i].value) <= 0) {
+      valid = false;
+      $(".error-msg").eq(i).css("visibility","visible");
+    }
+  }
+  return valid;
+}
+
+function solve24() {
+  //First, get all permutations of the inputted numbers
+  const nums = [$("#num1")[0].value,$("#num2")[0].value,$("#num3")[0].value,$("#num4")[0].value];
+  const permutations = permute(nums);
   
   //Iterate through each operation
   var ops = ['+','+','+']
@@ -96,4 +110,27 @@ function solve24 (numStr) {
 
   return true;
 }
-solve24("4878");
+
+
+/*
+ * Find all permutations of the given array.
+ */  
+function permute(arr){
+  var allPerms = [];
+  if(arr.length < 2) {
+    allPerms = [arr];
+  }
+  else {
+    //Put each term at the beginning, then follow it with each permute of the rest
+    for(var i = 0; i < arr.length; i++) {
+      var firstTerm = arr.splice(i,1)[0];
+      var nextPermutes = permute(arr);  
+      for (perm of nextPermutes) {
+        var newPerm = [firstTerm, ...perm];
+        allPerms.push([...newPerm]);
+      }
+    arr.splice(i,0,firstTerm);
+    }
+  }
+  return allPerms;
+};
